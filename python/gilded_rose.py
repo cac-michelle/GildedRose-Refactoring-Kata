390d98bcf75886d1b3abceb2c9364d7574488432
cac-michelle <michelle.chen@104.com.tw>
2022-03-04 16:08:03 +0800

    move decrease/increase quality into Item

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,36 @@
 # -*- coding: utf-8 -*-
 
+from inspect import _void
+
+
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
-    
-    def decrease_quality(self, item):
-        if item.quality > 0:
-            item.quality = item.quality - 1
-
-    def increase_quality(self, item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
-
+        
     def update_quality(self):
         for item in self.items:
             if item.name == "Sulfuras, Hand of Ragnaros":
                 return
-            item.sell_in = item.sell_in - 1
             if item.name == "Aged Brie":
-                self.increase_quality(item)
+                item.sell_in = item.sell_in - 1
+                item.increase_quality()
                 if item.sell_in < 0:
-                    self.increase_quality(item)
+                    item.increase_quality()
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                self.increase_quality(item)
+                item.sell_in = item.sell_in - 1
+                item.increase_quality()
                 if item.sell_in < 10:
-                    self.increase_quality(item)
+                    item.increase_quality()
                     if item.sell_in < 5:
-                        self.increase_quality(item)
+                        item.increase_quality()
                 if item.sell_in < 0:
                     item.quality = 0
             else:
-                self.decrease_quality(item)
+                item.sell_in = item.sell_in - 1
+                item.decrease_quality()
                 if item.sell_in < 0:
-                    self.decrease_quality(item)
+                    item.decrease_quality()
 
 
 class Item:
@@ -44,3 +41,16 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    
+    def decrease_quality(self):
+        if self.quality > 0:
+            self.quality = self.quality - 1
+
+    def increase_quality(self):
+        if self.quality < 50:
+            self.quality = self.quality + 1
+    
+    def update_quality(self):
+        _void
+
+    
